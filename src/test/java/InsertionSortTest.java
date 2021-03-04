4b54b1ff4843c9aef879427d4786cb53f4cd08b7
kirill <kirillbobryakov82@gmail.com>
2021-03-04 17:23:54 +0800

    Add test for insertion sorting

--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -11,17 +12,29 @@ public class InsertionSortTest {
 
     static InsertionSort insertionSort;
     static List<Integer> listStart = Arrays.asList(353, 104, 189, 950, 610, 196,  46, 469, 896, 349, 816, 526, 876, 151, 555, 984, 454, 162, 959, 712);
+    static List<Integer> sortedList = new ArrayList<>();
     static List<Integer> expectedList = Arrays.asList(46, 104, 151, 162, 189, 196, 349, 353, 454, 469, 526, 555, 610, 712, 816, 876, 896, 950, 959, 984);
+
 
     @BeforeAll
     static void setUp() {
         insertionSort = new InsertionSort();
-        insertionSort.sortList(listStart);
+        sortedList.addAll(listStart);
+        insertionSort.sortList(sortedList);
     }
 
     @Test
-    public void testInsertionSort() {
-        Assertions.assertArrayEquals(listStart.toArray(), expectedList.toArray());
+    public void testManualInsertionSort() {
+        Assertions.assertArrayEquals(expectedList.toArray(), sortedList.toArray());
     }
+
+    @Test
+    public void testAutoInsertionSort() {
+        List<Integer> copyStartList = new ArrayList<>(listStart);
+        Collections.sort(copyStartList);
+        Assertions.assertArrayEquals(copyStartList.toArray(), sortedList.toArray());
+    }
+
+
 
 }
